a64d29e8d5b802f962910dec95bcbf69e660d1cd
StefanK <57302957+StefanKarasmilov@users.noreply.github.com>
2022-03-18 08:37:44 +0100

    first commit

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+export const api = {
+  characters: async () => {
+    const response = await fetch('http://localhost:3000/characters.json')
+    return await response.json() as Character[]
+  },
+
+  comics: async (characterId: number) => {
+    const response = await fetch(`/comics-${characterId}.json`)
+    return await response.json() as Comic
+  },
+
+  allComics: async () => {
+    const response = await fetch('/comics.json')
+    return await response.json() as Comic[]
+  }
+}
+
+export interface Character {
+  id: number
+  name: string
+}
+
+export interface Comic {
+  id: number
+  title: string
+  characters: string[]
+}
